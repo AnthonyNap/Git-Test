89fa625cc315a69f8f90eac3a31edb9f91cfad00
AnthonyNap <Delaurian@outlook.fr>
2024-01-10 11:29:12 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let prenom = "Nap";
+let agepro = 39;
+let poidsl = 80;
+
+console.log(`Votre prénom est ${prenom}!`);
+console.log(`Votre age ${agepro} ans!`);
+console.log(`Votre poids est de ${poidsl} kg!`)
